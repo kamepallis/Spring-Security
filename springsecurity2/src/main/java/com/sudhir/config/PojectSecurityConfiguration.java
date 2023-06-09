adf36062544434c913f01f29450e11c0bc4d8e5f
unknown <kamepalli.sudhir@gmail.com>
2023-06-09 18:55:03 +0530

    added custom security configuration

---
+++ springsecurity2/src/main/java/com/sudhir/config/PojectSecurityConfiguration.java
@@ -0,0 +1,17 @@
+package com.sudhir.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class PojectSecurityConfiguration {
+
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().anyRequest().authenticated();
+        http.formLogin();
+        http.httpBasic();
+        return http.build();
+    }
+
+}
